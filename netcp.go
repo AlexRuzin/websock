eb1b3cd11244bb446cc39754ffa560da94f2e677
AlexRuzin <stan.ruzin@gmail.com>
2017-11-12 19:48:27 -0500

    Added server handler code which will parse the b64([8byte XOR key][XOR-encoded marshalled ECDH client pub key][md5sum of 1st 2 elements]. Appears to be a bug in the HTTP transmitter.

--- netcp.go
+++ netcp.go
@@ -35,7 +35,8 @@ import (
     "hash/crc64"
     "crypto/md5"
     "encoding/base64"
-    "os"
+    "time"
+    "io"
 )
 
 /*
@@ -68,14 +69,72 @@ type NetChannelService struct {
     PathGate string
 }
 
+// Server processor methods
+type ServerProcessor struct {}
+
 /* Create circuit -OR- process gate requests */
 func requestHandlerGate(writer http.ResponseWriter, reader *http.Request) {
+    serverProcessor := ServerProcessor{}
+
+    body_raw, err := reader.GetBody()
+    if err != nil {
+        serverProcessor.sendBadErrorCode(writer, err)
+        return
+    }
+    body_raw_vector := make([]byte, reader.ContentLength)
+    body_raw.Read(body_raw_vector)
+    marshalled, err := serverProcessor.getMarshalledPubKey(body_raw_vector)
+    if err != nil || len(marshalled) == 0 {
+        serverProcessor.sendBadErrorCode(writer, err)
+        return
+    }
+}
+
+func (ServerProcessor) getMarshalledPubKey(buffer []byte) (marshalled []byte, err error) {
     /*
      * Read in an HTTP request in the following format:
-     *  [8 bytes XOR key][XOR-SHIFT encrypted marshalled public ECDH key][md5sum of first 2]
+     *  b64([8 bytes XOR key][XOR-SHIFT encrypted marshalled public ECDH key][md5sum of first 2])
      */
-    util.DebugOut("[+] Request received....")
-    os.Exit(0)
+    b64_decoded, err := base64.StdEncoding.DecodeString(string(buffer))
+    if err != nil {
+        return nil, err
+    }
+
+    var raw_buffer = bytes.Buffer{}
+    raw_buffer.Write(b64_decoded)
+
+    var xor_key = make([]byte, 8)
+    r, err := raw_buffer.Read(xor_key)
+    if err != nil || r != len(xor_key) {
+        return nil, err
+    }
+
+    var marshal_buf = make([]byte, raw_buffer.Len() - md5.Size)
+    r, err = raw_buffer.Read(marshal_buf)
+    if err != nil || r != (len(b64_decoded) - md5.Size) {
+        return nil, err
+    }
+
+    var sum = make([]byte, md5.Size)
+    r, err = raw_buffer.Read(sum)
+    if err != io.EOF || r != md5.Size {
+        return nil, err
+    }
+
+    return nil, nil
+}
+
+/* HTTP 500 - Internal Server Error */
+func (ServerProcessor) sendBadErrorCode(writer http.ResponseWriter, err error) {
+    writer.WriteHeader(http.StatusInternalServerError)
+    writer.Write([]byte("500 - " + err.Error()))
+    return
+}
+
+/* HTTP 200 OK */
+func (ServerProcessor) sendGoodErrorCOde(writer http.ResponseWriter) {
+    writer.WriteHeader(http.StatusOK)
+    return
 }
 
 func CreateNetCPServer(path_gate string, port int16, flags int) (*NetChannelService, error) {
@@ -155,8 +214,6 @@ func (f *NetChannelClient) InitializeCircuit() error {
     } else {
         POST_STRING = METHOD_GET
     }
-    const CONTENT_TYPE_STRING = "Content-Type"
-    const CONTENT_TYPE_VALUE_STRING = "application/x-www-form-urlencoded"
 
     post_pool, err := f.genTxPool()
     if err != nil || len(post_pool) < 1 {
@@ -165,19 +222,30 @@ func (f *NetChannelClient) InitializeCircuit() error {
 
     form := url.Values{}
     form.Add(POST_PARAM_NAME, base64.StdEncoding.EncodeToString(post_pool))
-    form.Encode()
+    //form.Encode()
 
-    hc := http.Client{}
-    req, err := http.NewRequest(POST_STRING, f.InputURI, strings.NewReader(form.Encode()))
-    if err != nil {
-        return err
+    /* Perform HTTP TX */
+    resp, tx_err := func(method string, URI string, body io.Reader) (response *http.Response, err error) {
+        hc := http.Client{}
+        req, err := http.NewRequest(POST_STRING, f.InputURI, strings.NewReader(form.Encode()))
+        if err != nil {
+            return nil, err
+        }
+
+        req.Header.Add("Content-Type", "text/html")
+
+        resp, err := hc.Do(req)
+        if err != nil {
+            return nil, err
+        }
+
+        return resp, nil
+    } (POST_STRING, f.InputURI, strings.NewReader(form.Encode()))
+    if tx_err != nil {
+        return tx_err
     }
 
-    req.Header.Add(CONTENT_TYPE_STRING, CONTENT_TYPE_VALUE_STRING)
-    resp, err := hc.Do(req)
-    if err != nil {
-        return err
-    }
+    time.Sleep(1000)
 
     if resp.Status != "200 OK" {
         return errors.New("HTTP 200 OK not returned")
@@ -196,10 +264,10 @@ func (f *NetChannelClient) genTxPool() ([]byte, error) {
         return nil, err
     }
 
-    /******************************************************************************************
-     * Tranmis the public key ECDH key to server. The transmission buffer contains:           *
-     *  [8 bytes XOR key][XOR-SHIFT encrypted marshalled public ECDH key][md5sum of first 2]  *
-     ******************************************************************************************/
+    /***********************************************************************************************
+     * Tranmis the public key ECDH key to server. The transmission buffer contains:                *
+     *  b64([8 bytes XOR key][XOR-SHIFT encrypted marshalled public ECDH key][md5sum of first 2])  *
+     ***********************************************************************************************/
     var pubKeyMarshalled = curve.Marshal(clientPublicKey)
     var pool = bytes.Buffer{}
     tmp := make([]byte, crc64.Size)
@@ -219,7 +287,8 @@ func (f *NetChannelClient) genTxPool() ([]byte, error) {
     pool_sum := md5.Sum(pool.Bytes())
     pool.Write(pool_sum[:])
 
-    return pool.Bytes(), nil
+    b64_buf := base64.StdEncoding.EncodeToString(pool.Bytes())
+    return []byte(b64_buf), nil
 }
 
 func configCheck() error {
