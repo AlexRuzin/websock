f7db159a96e86f05ed14589e39d3c02d0ec1a393
AlexRuzin <stan.ruzin@gmail.com>
2018-04-01 21:53:44 -0400

    Fixed the nix startup script

--- src/start_remote_server.sh
+++ src/start_remote_server.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-SRC_DIR="`pwd`/src"
-JSON_CONFIG="config.remote.server.json"
+SRC_DIR="`pwd`"
+JSON_CONFIG="`pwd`/../config/config.remote.server.json"
 
-go test -v $SRC_DIR/atomic.go $SRC_DIR/pke.go $SRC_DIR/config.go $SRC_DIR/controller.go $SRC_DIR/websock_test.go -args -config $JSON_CONFIG 
+go clean
+go build
+
+go test -v $SRC_DIR/client.go $SRC_DIR/server.go $SRC_DIR/pke.go $SRC_DIR/config.go $SRC_DIR/shared.go $SRC_DIR/websock_test.go -args -config $JSON_CONFIG 
 
